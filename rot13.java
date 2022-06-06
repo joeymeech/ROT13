30291c3fa7866177987b83bccf2b4b721a232728
joeymeech <32067575+joeymeech@users.noreply.github.com>
2022-06-05 23:21:33 -0400

    Update rot13.java

--- rot13.java
+++ rot13.java
@@ -2,40 +2,11 @@ import java.util.Scanner;
 
 public class rot13{
 
-	private String encrypted = "";
-	private String decrypted = "";
+	private int ascii;
 
-	public String encryptString(String plainText){
-		int ascii;
-		for(char c : plainText.toCharArray()){
-			ascii = (int) c;
-			if(Character.isLetter(c)){
-				ascii += 13;
-				if(Character.isUpperCase(c)){
-					// if there is wrap around
-					if(ascii > 90){
-						// find remandiner of wrap around and add to base capital letter(65)
-						ascii = (ascii%91) + 65;
-					}
-				}
-				else if(Character.isLowerCase(c)){
-					if(ascii > 122){
-						ascii = (ascii%123) + 97;
-					}
-				}
-				c = (char) ascii;
-			}
-			encrypted = encrypted + c;
-		
-		}
-		System.out.println("Your encrypted text: ");
-		return encrypted;	
-	
-	}
-
-	public String decryptString(String enryptedText){
-		int ascii;
-		for(char c : enryptedText.toCharArray()){
+	public String cryptString(String text){
+		String decrypted = "";
+		for(char c : text.toCharArray()){
 			ascii = (int) c;
 			if(Character.isLetter(c)){
 				ascii -= 13;
@@ -55,7 +26,7 @@ public class rot13{
 			decrypted = decrypted + c;
 		
 		}
-		System.out.println("Your decrypted text: ");
+		System.out.println("Your encrypted/decrypted text: ");
 		return decrypted;	
 	
 	}
@@ -68,23 +39,17 @@ public class rot13{
 		System.out.println("This script encrypts and decrpts your text using the rot13 cipher(Shout out the romans)");
 		while (check){
 			System.out.println("Select a number to contine: ");
-			System.out.println("[1] encrypt text");
-			System.out.println("[2] decrypt text");
-			System.out.println("[3] quit");
+			System.out.println("[1] encrypt/decrypt text");
+			System.out.println("[2] quit");
 			int num = sc.nextInt();
 			sc.nextLine();
 			switch(num){
 				case 1:
 					System.out.print("Enter your text: ");
-					String plainText = sc.nextLine();
-					System.out.println(cipher.encryptString(plainText) + "\n");
+					String text = sc.nextLine();
+					System.out.println(cipher.cryptString(text) + "\n");
 					break;
 				case 2:
-					System.out.print("Enter your text: ");
-					String enryptedText = sc.nextLine();
-					System.out.println(cipher.decryptString(enryptedText) + "\n");
-					break;
-				case 3:
 					sc.close();
 					check = false;
 					break;
